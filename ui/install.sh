82fbcb71a1d11b4a92368a39825ead455e07a14d
Charles Nguyen <nguyencharles@live.ca>
2022-12-10 17:04:52 -0500

    fix polybar directory creation

--- ui/install.sh
+++ ui/install.sh
@@ -8,6 +8,10 @@ echo "Installing UI libraries..."
 # Install Polybar
 yay polybar --noconfirm
 
+echo "Setting up Polybar directory..."
+mkdir $HOME/.config/polybar
+touch $HOME/.config/polybar config.ini launch.sh
+
 echo "Symlink polybar configurations..."
 ln -sf ~/linux-setup/ui/polybar/config.ini ~/.config/polybar/config.ini
 ln -sf ~/linux-setup/ui/polybar/launch.sh ~/.config/polybar/launch.sh
