ba5d7e7e0c937bd356ee5908d3b42081e6b7b76b
Charles Nguyen <nguyencharles@live.ca>
2022-12-10 11:33:38 -0500

    add fonts and update polybar config

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,18 +1,10 @@
-#!/bin/bash
+echo "Installing fonts..."
 
-echo "Skipping fonts installation..."
 
-##########################################################
+echo "1) Installing JetBrain Mono"
+yay --noconfirm nerd-fonts-jetbrains-mono
 
-# echo "Installing fonts"
-# echo "----------------"
-# echo "1) Installing SF Mono"
-# cp /Applications/Utilities/Terminal.app/Contents/Resources/Fonts/*.otf ~/Library/Fonts/ 
-# echo "3) Installing fonts from cask"
-# brew install --cask font-hack-nerd-font
-# brew install --cask font-cascadia
-# brew install --cask font-jetbrains-mono
-# brew install --cask font-fira-code-nerd-font
-# brew install --cask font-iosevka
+echo "1) Installing Iosevka"
+yay --noconfirm ttf-iosevka
 
 # echo "Done!"
